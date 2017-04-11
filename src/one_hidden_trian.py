c0c29bbeb43eedea4469985cfa941b45103d19ab
zqhZY <zqingheng@163.com>
2017-04-11 11:22:36 +0800

    reg nnet

--- src/one_hidden_trian.py
+++ src/one_hidden_trian.py
@@ -4,6 +4,7 @@
 LAYER_NODE1 = 500 # layer1 node num
 INPUT_NODE = 5000
 OUTPUT_NODE = 10
+REG_RATE = 0.01
 
 import tensorflow as tf
 from datasets import datasets
@@ -32,7 +33,12 @@ b2 = tf.Variable(tf.constant(0.0, shape=[OUTPUT_NODE]))
 y = interface(x, w1, b1, w2, b2)
 
 cross_entropy = -tf.reduce_sum(y_ * tf.log(y + 1e-10))
-train_step = tf.train.GradientDescentOptimizer(0.01).minimize(cross_entropy)
+regularizer = tf.contrib.layers.l2_regularizer(REG_RATE)
+regularization = regularizer(w1) + regularizer(w2)
+loss = cross_entropy + regularization
+
+
+train_step = tf.train.GradientDescentOptimizer(0.01).minimize(loss)
 
 #training
 tf.global_variables_initializer().run()
@@ -42,13 +48,13 @@ cv_feed = {x: data_sets.cv.text, y_: data_sets.cv.label}
 correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
 acc = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
-for i in range(4000):
+for i in range(5000):
 	if i % 200 == 0:
 		cv_acc = sess.run(acc, feed_dict=cv_feed)
 		print "train steps: %d, cv accuracy is %g " % (i, cv_acc)
-	batch_xs, batch_ys = data_sets.train.next_batch(120)
+	batch_xs, batch_ys = data_sets.train.next_batch(100)
 	train_step.run({x: batch_xs, y_: batch_ys})
 
-path = saver.save(sess, "./model5/model.md")
+path = saver.save(sess, "./model4/model.md")
 
 
